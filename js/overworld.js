77d7b158b487987245b1241e61ff1d4ca900a0ae
andrewwinke123 <andrewwinke@gmail.com>
2023-06-16 19:46:59 -0400

    initialize overworld and test first image

--- js/overworld.js
+++ js/overworld.js
@@ -1,10 +1,14 @@
 class Overworld {
-  constructor() {
+  constructor(config) {
     this.element = config.element
     this.canvas = this.element.querySelector('.game-canvas')
     this.ctx = this.canvas.getContext('2d')
   }
   init() {
-    console.log('Overworld test!', this)
+    const image = new Image()
+    image.onload = () => {
+      this.ctx.drawImage(image,0,0)
+    }
+    image.src = '/img/maps/field-2.png'
   }
 }
