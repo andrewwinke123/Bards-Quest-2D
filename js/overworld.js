85ac92797f4d9e2c3ed0ee78da6a9dad3cfbee1f
andrewwinke123 <andrewwinke@gmail.com>
2023-06-16 20:50:51 -0400

    creating the main character from custom  sprite sheet

--- js/overworld.js
+++ js/overworld.js
@@ -10,5 +10,24 @@ class Overworld {
       this.ctx.drawImage(image,0,0)
     }
     image.src = '/img/maps/field-2.png'
+    
+    const x = 0
+    const y = 0
+    const hero = new Image()
+    hero.onload = () => {
+      this.ctx.drawImage(
+        hero, 
+        0, // left
+        0, // top
+        32, // width
+        32, // height
+        x,
+        y,
+        32,
+        32
+      )
+    }
+    hero.src = '/img/characters/people/little-goblin.png'
+
   }
 }
